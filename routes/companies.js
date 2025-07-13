c4d91f890bf74c47fa1abc08a3f20398faf89ef1
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2025-07-13 19:27:50 +0900

    [Feat] Companies 기능 추가 (repository, service, controller, router)

---
+++ routes/companies.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const companiesController = require("../controller/companiesController");
+
+// 전체 기업 목록 또는 검색
+router.get("/", companiesController.getCompanies);     // /api/companies
+
+module.exports = router;
