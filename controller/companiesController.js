f46784366a73304412b432baa57ac302bc227be1
JeongHyeon Jo <cisxo@naver.com>
2025-07-15 15:23:55 +0900

    refactor(companies): divided search api

--- controller/companiesController.js
+++ controller/companiesController.js
@@ -1,5 +1,7 @@
 const companyService = require("../service/companiesService");
 
+
+// /api/companies
 exports.getCompanies = async (req, res) => {
     try {
         const companies = await companyService.fetchCompanies(req.query);
@@ -9,3 +11,14 @@ exports.getCompanies = async (req, res) => {
         res.status(500).json({ success: false, message: "기업 정보를 불러올 수 없습니다." });
     }
 };
+
+//  /api/companies/search
+exports.getSearchCompanies = async (req, res) => {
+    try {
+        const companies = await companyService.fetchCompanies(req.query);
+        res.json({ success: true, data: companies });
+    } catch (err) {
+        console.error("Company fetch error:", err);
+        res.status(500).json({ success: false, message: "기업 정보를 불러올 수 없습니다." });
+    }
+}
